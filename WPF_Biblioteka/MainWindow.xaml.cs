2046560aa48876b8dded514b30117753681211ee
Mayweather50 <62692570+Mayweather50@users.noreply.github.com>
2022-03-03 22:11:08 +0300

    Project

---
+++ WPF_Biblioteka/MainWindow.xaml.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_Biblioteka
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Main.Content = new Book();
+        }
+
+      
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+                this.DragMove();
+
+
+
+        }
+
+        private void ChangeWindowState()
+        {
+            if (WindowState == WindowState.Normal)
+            {
+                this.WindowState = WindowState.Maximized;
+                Window_Edge.Visibility = Visibility.Collapsed;
+            }
+            else
+            {
+                if (WindowState == WindowState.Maximized) 
+                {
+                    this.WindowState = WindowState.Normal;
+                    Window_Edge.Visibility = Visibility.Visible;
+
+                }
+            }
+        }
+
+        private void Main_Window_Panel_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left && e.ClickCount == 2)
+            {
+                ChangeWindowState();
+            }
+
+            //// Close All Open Menu Panels on Mouse Button Click
+            //if (e.ChangedButton == MouseButton.Left && e.ClickCount == 1)
+            //{
+            //    //// Access the SubmenuOne UserControl Account Btn and Unselect it. 
+            //    //if (Application.Current.MainWindow is MainWindow MW)
+            //    //{
+            //    //    MW.SubmenuOne_UC.account_btn.IsSelected = false;
+            //    //}
+
+            //    //// Access the MainMenu UserControl Menu Toggle Btn and Uncheck it. 
+            //    //if (Application.Current.MainWindow is MainWindow MW1)
+            //    //{
+            //    //    MW1.MainMenu_UC.Menu_ToggleButton.IsChecked = false;
+            //    //}
+
+            //    //// Access the SidePanel UserControl Mail Btn and Unselect it. 
+            //    //if (Application.Current.MainWindow is MainWindow MW2)
+            //    //{
+            //    //    MW2.MainMenu_UC.SidePanel_UC.mail_sidepanel_listboxitem.IsSelected = false;
+            //    //}
+
+            //}
+        }
+
+        private void ButtonFechar_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void Polygon_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+        }
+
+        private void minimizeButton_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+
+        private void maximizeButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (WindowState == WindowState.Normal)
+                WindowState = WindowState.Maximized;
+            else
+                WindowState = WindowState.Normal;
+
+        }
+
+        private void closeButton_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Autorize autorize = new Autorize();
+            autorize.Show();
+        }  
+        
+        
+        private void Button_Click_Books(object sender, RoutedEventArgs e)
+        {
+            Main.Content = new Book();
+         
+        }
+
+
+
+    }
+}
---
+++ WPF_Biblioteka/SidePanel.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_Biblioteka
+{
+    /// <summary>
+    /// Логика взаимодействия для SidePanel.xaml
+    /// </summary>
+    public partial class SidePanel : UserControl
+    {
+        public SidePanel()
+        {
+            InitializeComponent();
+        }
+
+        private void mail_sidepanel_listboxitem_Selected(object sender, RoutedEventArgs e)
+        {
+            // Start the SubmenuOne Slide-In Animation 
+            Storyboard sb = FindResource("SubmenuOne_Enter") as Storyboard;
+            sb.Begin();
+
+            // Access the SubmenuOne UserControl Menu Panel and Enable it. 
+           
+
+            // TrackOne Enter Indicator
+           
+
+        }
+
+        private void mail_sidepanel_listboxitem_Unselected(object sender, RoutedEventArgs e)
+        {
+            // Start the SubmenuOne Slide-Out Animation 
+            Storyboard sb = FindResource("SubmenuOne_Exit") as Storyboard;
+            sb.Begin();
+
+            // TrackOne Exit Indicator 
+          
+        }
+
+      
+    }
+}
